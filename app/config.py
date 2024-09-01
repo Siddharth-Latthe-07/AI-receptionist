27e14347fa3bbedb54a62f6fe8c361c2ad38d652
Siddharth Latthe <121747151+Siddharth-Latthe-07@users.noreply.github.com>
2024-09-01 20:09:40 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
 # config.py
-QDRANT_HOST = "localhost"
-QDRANT_PORT = 6333
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
+QDRANT_HOST = os.getenv("QDRANT_HOST", "localhost")
+QDRANT_PORT = int(os.getenv("QDRANT_PORT", 6333))
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
